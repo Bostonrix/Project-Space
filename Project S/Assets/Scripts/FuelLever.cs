5c984a2f4d0373ef26b1e6d67665d7475e4b6159
Bostonrix <79361812+Bostonrix@users.noreply.github.com>
2024-07-04 18:26:10 +1000

    Final Commit for sample scene

---
+++ Project S/Assets/Scripts/FuelLever.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FuelLever : MonoBehaviour
+{
+    Animator ani;
+    bool triggered = false;
+    public void Start(){
+        // outline = gameObject.GetComponent<Outline>(); get outline shader on gameobject
+        // outline.enabled = false; set the ouline to false
+        ani = gameObject.GetComponentInParent<Animator>();
+    }
+   //detects if the player looks at object
+    public void OnLook(){
+
+    }
+
+    //detects if the player looks away from object
+public void OnLookAway(){
+    // outlines.enabled = false;
+}
+
+   //Detects if the player clicks object
+    public void buttonPressed()
+    {
+        triggered = true;
+        if(triggered == true)
+        {
+            ani.SetTrigger("Pushed");
+        } //plays buttonpressed animation
+
+    }
+
+    public void buttonReleased(){
+        triggered = false;
+        if (triggered == false)
+        {
+            ani.SetTrigger("Released");//resets trigger and stop the button animation
+        }
+    }
+}
