562b5d295b4c27a5b3966a0f5738f7e9ec3332fd
Luke Dunstan <85138406+LukeDunstan@users.noreply.github.com>
2024-07-06 17:38:32 +1000

    minor changes to HazardHandler script

--- Project S/Assets/HazardHandler.cs
+++ Project S/Assets/HazardHandler.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 public class HazardHandler : MonoBehaviour
 {
     public float setTimebetweenHazards = 60;
+    bool activeHazard;
     public float timebetweenHazards;
     ButtonTask[] buttonTasks;
     void Start()
     {
         buttonTasks = FindObjectsOfType<ButtonTask>();
         timebetweenHazards = setTimebetweenHazards;
+        activeHazard = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timebetweenHazards -= 1*Time.deltaTime;
-        if (timebetweenHazards <= 0){
-            beginHazard();
+        if (activeHazard == false){
+            timebetweenHazards -= 1*Time.deltaTime;
+            if (timebetweenHazards <= 0){
+                beginHazard();
+            }
         }
     }
 
     public void hazardCleared(){
+        activeHazard = false;
         timebetweenHazards = setTimebetweenHazards;
         GetComponentInParent<EnvironmentHandler>().hazardCleared();
     }
 
     void beginHazard (){
-        ButtonTask current = buttonTasks[Random.Range(0,buttonTasks.Length-1)]; // randomly selects detected task buttons
+        activeHazard = true;
+        ButtonTask current = buttonTasks[Random.Range(0,buttonTasks.Length-1)]; // randomly selects detected task buttons (doesn't seem to be random at the moment)
         current.startTask(); //starts task on selected 
         GetComponentInParent<EnvironmentHandler>().disarray(); //sends environment into Disarray
     }
